edb60ed784632dfc4ac6e3d7a6f86f26b6fd0fbf
Yoku <yungyoku@yandex.ru>
2025-07-14 11:59:39 +0300

    Updated baskets buy route

--- db/pb_hooks/payForTheShoppingCart.pb.js
+++ db/pb_hooks/payForTheShoppingCart.pb.js
@@ -3,9 +3,26 @@ routerAdd('POST', '/api/baskets/pay', (c) => {
 	const user = $app.findAuthRecordByToken(token, 'auth')
 	const baskets = user.get('baskets')
 
+	const calculateTotalPrice = () => {
+		let result = 0
+		for (const cart of baskets) {
+			const fullCart = $app.findRecordById('baskets', cart)
+
+			if (fullCart.get('status') !== 'created') continue
+
+			const creativeId = fullCart.get('creative')
+			const creative = $app.findRecordById('creatives', creativeId)
+
+			result += creative.get('price') * fullCart.get('geo').length
+		}
+		return result
+	}
+
 	const updateShoppingCarts = () => {
 		for (const cart of baskets) {
 			const fullCart = $app.findRecordById('baskets', cart)
+
+			if (fullCart.get('status') !== 'created') continue
 
 			fullCart.set('status', 'pending')
 			const cartGeo = fullCart.get('geo')
@@ -22,9 +39,18 @@ routerAdd('POST', '/api/baskets/pay', (c) => {
 		}
 	}
 
-	updateShoppingCarts()
+	const totalPrice = calculateTotalPrice()
+
+	const updateUserBalance = () => {
+		user.set('balance', user.get('balance') - totalPrice)
+		$app.save(user)
+	}
+
+	if (user.get('balance') > totalPrice) {
+		updateShoppingCarts()
+		updateUserBalance()
+	}
 
 	$app.expandRecord(user, ['baskets', 'baskets.creative', 'baskets.creative.preview', 'baskets.creative.slot', 'baskets.geo'], null)
-
 	return c.json(200, user)
 })
