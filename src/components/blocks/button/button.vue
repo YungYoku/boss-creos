528de1f3752bbd532e214d7016d9371a5dcab145
YungYoku <yungyoku@yandex.ru>
2025-08-03 12:50:51 +0300

    Updated profile edit

--- src/components/blocks/button/button.vue
+++ src/components/blocks/button/button.vue
@@ -99,8 +99,9 @@ const props = defineProps({
 	}
 
 	&.outline {
-		background-color: hsl(var(--background));
-		border: 1px solid hsl(var(--input));
+		background: linear-gradient(180deg, rgba(255, 255, 255, 0.1) 0%, rgba(255, 255, 255, 0.08) 100%),
+		radial-gradient(50% 100% at 50% 0%, rgba(255, 255, 255, 0.1) 0%, rgba(255, 255, 255, 0) 100%);
+
 
 		&:hover {
 			background-color: hsl(var(--accent));
--- src/views/profile/profile.vue
+++ src/views/profile/profile.vue
@@ -1,41 +1,47 @@
 <template>
-	Редактировать
-	<Grid
-		:columns="1"
-		vertical
-		class="profile"
-	>
-		<Grid :columns="2">
-			<InputRich
-				v-model="form.username.value"
-				:error="form.username.error"
-				:disabled="loading"
-				label="Имя"
-			/>
-
-			<InputImageRich
-				v-model="form.avatar.value"
-				:error="form.avatar.error"
-				:disabled="loading"
-				label="Аватар"
-			/>
-		</Grid>
-
-		<TextareaRich
-			v-model="form.description.value"
-			:error="form.description.error"
-			:disabled="loading"
-			label="О себе"
-			height="240px"
-		/>
-
-		<Button
-			:disabled="loading"
-			@click="save"
+	<Island class="profile">
+		<Grid
+			:columns="1"
+			vertical
 		>
-			Сохранить
-		</Button>
-	</Grid>
+			<div class="profile__title">
+				Редактировать
+			</div>
+
+			<Grid :columns="2">
+				<InputRich
+					v-model="form.username.value"
+					:error="form.username.error"
+					:disabled="loading"
+					label="Имя"
+				/>
+
+				<InputImageRich
+					v-model="form.avatar.value"
+					:error="form.avatar.error"
+					:disabled="loading"
+					label="Аватар"
+				/>
+			</Grid>
+
+			<TextareaRich
+				v-model="form.description.value"
+				:error="form.description.error"
+				:disabled="loading"
+				label="О себе"
+				height="240px"
+			/>
+
+			<Button
+				class="profile__save"
+				:disabled="loading"
+				variant="outline"
+				@click="save"
+			>
+				Отправить на модерацию
+			</Button>
+		</Grid>
+	</Island>
 </template>
 
 <script setup lang="ts">
@@ -47,6 +53,7 @@ import { Form, Http } from '@/plugins'
 import { Grid } from '@/components/structures'
 import { Button, InputImageRich, InputRich, TextareaRich } from '@/components/blocks'
 import { emptyUser, IUser } from '@/interfaces/User.ts'
+import Island from '@/components/structures/island.vue'
 
 const auth = useAuthStore()
 const toast = useToast()
@@ -79,6 +86,17 @@ const save = async () => {
 
 <style scoped lang="scss">
 .profile {
-	max-width: 768px;
+	width: 585px;
+
+	&__title {
+		text-align: center;
+		font-weight: 600;
+		font-size: 20px;
+	}
+
+	&__save {
+		width: 240px;
+		margin: 0 auto;
+	}
 }
 </style>
